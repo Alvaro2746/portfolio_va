d88bf0dbaebc53e3db78d9d7102689c153926436
Alvaro2746 <alvaroviyareal@gmail.com>
2023-11-18 09:06:25 -0500

    Cambios

--- files/proyectos.php
+++ files/proyectos.php
@@ -9,7 +9,7 @@
     <title>Proyectos</title>
     <link rel="stylesheet" href="../css/bootstrap-5.3.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../css/estilos2.css">
     <link rel="stylesheet" href="../css/bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js">
 </head>
 
