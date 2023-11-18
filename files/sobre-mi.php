d5aab9f9270579eaaf897d23739c7adb021eb6f4
Alvaro2746 <alvaroviyareal@gmail.com>
2023-11-18 09:27:08 -0500

    cambios

--- files/sobre-mi.php
+++ files/sobre-mi.php
@@ -360,28 +360,8 @@
 
 
     </main>
-    <footer class="container mt-auto pb-2 pt-5">
+    <?php include_once("footer.php"); ?>
 
-        <div class="row">
-            <div id="i1" class="col-12 col-sm-3 text-center">
-                <a href="https://github.com/" target="_blank"><i class="fa-brands fa-github"></i></a>
-                <a href="https://www.linkedin.com/feed/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-            </div>
-            <div id="i2" class="col-12 col-sm-3 text-center">
-                <p>Student DePC Suite</p>
-            </div>
-            <div id="i3" class="col-12 col-sm-3 text-center">
-                <p>alvaroviyareal@gmail.com</p>
-            </div>
-            <div id="i4" class="col-3">
-                <div id="fix">
-                    <a href="https://api.whatsapp.com/send?phone=573184559655" target="_blank"><i
-                            class="fa-brands fa-whatsapp"></i></a>
-                </div>
-            </div>
-        </div>
-
-    </footer>
 </body>
 
 </html>
